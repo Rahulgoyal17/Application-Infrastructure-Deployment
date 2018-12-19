0d52e9f0022f318b697309e6ef19e63aacd6b681
Rahulgoyal17 <rahulg169@gmail.com>
2018-12-19 18:40:04 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ module "ec2_cluster" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "1.12.0"
 
-  name                   = "webserver"
+  name                   = "Application-Server"
   instance_count         = 1
 
   ami                    = "ami-009d6802948d06e52"
@@ -20,6 +20,7 @@ module "ec2_cluster" {
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = "PROD"
+    Application = "Hello-World"
   }
 }
